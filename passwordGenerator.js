51efda3b2de94768233c9eb1f9fa36427750338a
Alejandro Zuluaga Baron <Alejandro@Alejandros-iMac.local>
2023-04-04 15:25:16 -0500

    Basic JavaScript password generator

---
+++ passwordGenerator.js
@@ -0,0 +1,28 @@
+
+
+function generatePassword() {
+
+    const char = ["a", "b", "c", "d"]
+    const num = [1,2,3,4,5,6,7,8,9,0]
+    const arr = char.concat(num)
+
+    let emptyPassword = [];
+
+    for (let i = 0; i <= 9; i++ ) {
+
+        let randomNumber = Math.floor(Math.random() * 10);
+
+        emptyPassword.push(arr[randomNumber])
+
+        console.log(emptyPassword);
+    };
+
+    let string = emptyPassword.join("")
+
+    return string
+}
+
+let password = generatePassword()
+
+
+console.log(password)
